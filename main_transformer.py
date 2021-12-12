4f390ebfe527d65a41cbe96193f60e7e4f6f1104
NimrodShabtay <nimrod.shabtay@gmail.com>
2021-12-12 15:23:56 +0200

    wip

--- main_transformer.py
+++ main_transformer.py
@@ -110,7 +110,7 @@ if __name__ == '__main__':
         net = net.type(dtype)
 
     elif fname in ['data/denoising/F16_GT.png', 'data/inpainting/kate.png']:
-        num_iter = 10000
+        num_iter = 20000
         input_depth = 32
         figsize = 4
         net = get_net(input_depth, d['model'],
